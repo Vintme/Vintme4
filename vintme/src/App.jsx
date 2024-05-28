eed0ab904a46e5105c4f4017235b6c9f01521176
maximilianoguevara-gf <maximiliano.guevara@gojiraf.com>
2024-05-28 06:08:32 -0300

    first commit

---
+++ vintme/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    return (
+      <div className="construction-container">
+        <div className="content">
+          <h1>UNDER CONSTRUCTION</h1>
+          <img
+            src="https://i.ibb.co/dKtMz98/Logo-Vintme-Logo.png"
+            alt="Under Construction"
+          />
+        </div>
+      </div>
+    );
+  }
+  
+export default App
